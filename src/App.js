5f16cde44a29a7cebe6bbc92bb472bb592eea5bb
YoungJun <dudwns9073@nate.com>
2019-07-20 13:42:16 +0900

    initial_install

--- src/App.js
+++ src/App.js
@@ -26,32 +26,70 @@ import Movie from './Movie';
   
     componentDidMount() {
      console.log("componentDidMount");
-     // then function은 1개의 attribute만 준다.
-     // 그리고 fetch의 결과물은 오브젝트이다.   
-     fetch("https://yts.lt/api/v2/list_movies.json?sort_by=download_count")
-     .then(response => response.json())
-     .then(json => console.log(json))
-     .catch(err => console.log(err))
-    
+      /*
+        then function은 1개의 attribute만 준다.
+        그리고 fetch의 결과물은 오브젝트이다.   
+        If you want change a state 
+        ->  you may be has a CALLBACK HELL!!!
+        fetch("https://yts.lt/api/v2/list_movies.json?sort_by=download_count")
+        .then(response => response.json())
+        .then(json => console.log(json))
+        .catch(err => console.log(err))
+      */
+      this._getMovies();
     }
     
-    _rederMovies = () => {
-      const movies = this.state.movies.map((movie, index) => {
-        return <Movie title = {movie.title} poster={movie.poster} key ={index} />
+    // 이전 라인의 작업이 끝날때까지 기다리는 것이 아닐때
+    // asynchronous는 이전 작업이 끝나야 그 다음 작업이 시작하는 형태가 아니다.
+    // 순서와 상관없이 작업이 진행된다.
+    _getMovies = async () => {
+      // callApi의 return value를 movies에 set할거다.
+      // async를 안쓰면 await이 작동을 안한다.
+      const movies = await this._callApi();
+      // 이 컴포넌트의 setState을 movies로 할거다.
+      // movies는 callApi의 return value이다.
+      // 이 setState는 callApi작업이 완료되기(성공적 수행은 아니다.) 전 까지는 실행되지 않는다.
+      // 성공적 수행이 되든 실패가 된든 작업이 완료가 되어야 한다.
+      // 그 후에 실행이 된다.
+      this.setState({
+         movies
       });
-      return movies
+    };
+
+    _callApi  = () => {
+      return fetch("https://yts.lt/api/v2/list_movies.json?sort_by=download_count")
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err)) 
     }
 
+    _rederMovies = () => {
+      // component of index is very lose...
+      // very slowwwww...
+      const movies = this.state.movies.map((movie) => {
+        return (
+          <Movie 
+            title = {movie.title_english} 
+            poster= {movie.medium_cover_image} 
+            key = {movie.id} 
+            genres = {movie.genres} 
+            synopsis = {movie.synopsis}
+          />
+        );
+      });
+      return movies;
+    };
+
     render() {
+      const {movies} = this.state;
       return (
       // 자신이 찾고있는 데이터가 있느지 체크를 하고, 자바스크립트에게 물어본다.
       // 만약 참이면 _rederMovies()를 실행
       // 거짓이면 Loding을 출력
-
-      <div className="App">
-           {this.state.movies  ? this._rederMovies() : "Loading"}
-      </div>
-      ); 
+        <div className={movies ? "App" : "App--loading"}>
+          {movies ? this._rederMovies() : "Loading"}
+        </div>
+      );
     }
   }
 
--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,8 @@
 import React  from 'react';
 import './Movie.css';
 import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis'
+
 /*
     9. Smart vs Dumb Components 
     모든 컴포넌트가 state이 있는 것은 아니다.
@@ -8,46 +10,77 @@ import PropTypes from 'prop-types';
     
     이 두개의 컴포넌트는 큰 차이점이 있다.
     한 개는 state이 있고, 나머지 한 개는 state이 없고, 필요하지도 않다.
-    이것을 dumb(멍청한) 컴포넌트, smart(똑똑한) 컴포넌트라고 한다.
-    똑똑한 컴포넌트는 state이 있고, 멍청한 컴포넌트는 state이 없다.(갖고 있는 것은 props 뿐이다.)
-    state이 없고, props밖에 없을때는 클래스 컴포넌트를 쓰는 대신에
-    functional 컴포넌트로 바꾸면 된다. 
+    이것을 dumb(멍청한), smart(똑똑한) 컴포넌트라고 한다.
+    smart 컴포넌트는 state이 있고, 
+    dumb 컴포넌트는 state이 없다.(갖고 있는 것은 props 뿐이다.)
+    state이 없고, props밖에 없을때는 클래스 컴포넌트를 쓰는 대신에 functional 컴포넌트로 바꾸면 된다. 
     정확히 말하면, sateless functional 컴포넌트라고 한다.
     
     예를 들어, movie poster를 갖는 대신에 요소들이 필요없다고 하자.
     이를 stateless functional 컴포넌트로 바꾸는 방법은 function MoviePoster
 */
-
 /*
-    이 컴포넌트는 componentWillMount, function , update state 필요가 없다.
+    이 컴포넌트는 componentWillMount, function , update state가 필요없다.
     그냥 한개의 props만 있으면 된다. poster와 같이 
+   
     즉, state가 없다.
     function render가 없고, 라이프 사이클도 없다.
     갖고 있는것은 return 밖에 없다.
     state을 잃게 되어, 업데이트 하는 기능들은 사용을 못한다.Components
 */
-function Movie({title , poster}) {
+
+function Movie({title , poster, genres, synopsis}) {
     return( 
-        <div>
-            <h1>{title}</h1>
-            <MoviePoster poster={poster}/>
+        <div className = "Movie">
+            <div className ="Moview_Colum">
+                <MoviePoster poster={poster} alt={title}/>           
+            </div>
+            <div className="Movie_Colum">
+                <h1>{title}</h1>
+                <div className="Movie_Genres">
+                    {genres.map( (genre, index) => <MovieGeners genre={genre} key={index}/>)}
+                    <p className="Movie_Synopsis">
+                        <LinesEllipsis
+                            text={synopsis}
+                            maxLine='3'
+                            ellipsis='...'
+                            trimRight
+                            basedOn='letters'
+                        />   
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
 
-function MoviePoster({poster}) {
+function MoviePoster({poster, alt}) {
     return (
-        <img src={poster} alt="Movie Poster" />
+        <img src={poster} alt={alt} title={alt} className="Movie_Poster"/>
+    )
+}
+
+// 컴포넌트를 생성한 이유는 더 세련된 코딩방식이다.
+function MovieGeners({genre}) {
+    return(
+        <span className = "Movie_Genre">{genre}</span>
     )
 }
 
 //  prop types 확인
 MoviePoster.propTypes = {
-    poster : PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+}
+
+MovieGeners.propTypes = {
+    genre: PropTypes.string.isRequired
 }
 export default Movie;
